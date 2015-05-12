15a13555913dff70ed2bdde527c8bddef23c06fc
Geoffrey Nguyen <geoffrey.nguyen@hotmail.fr>
2015-05-12 12:48:07 +0200

    formulaire

--- src/TT/TravelBundle/Controller/MainController.php
+++ src/TT/TravelBundle/Controller/MainController.php
@@ -3,11 +3,22 @@
 namespace TT\TravelBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends Controller
 {
     public function indexAction()
     {
         return $this->render('TTTravelBundle:Main:index.html.twig');
+    }
+    
+    public function searchAction(Request $request)
+    {
+        if($request->getMethod() == 'POST'){
+            var_dump('test');
+            die;
+        }
+        
+        return $this->render('TTTravelBundle:Main:form.html.twig');
     }
 }
